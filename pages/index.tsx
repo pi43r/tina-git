72f7d5635e0ced86b766a83ab01f1d2cbaebb257
pitscher <schaefer9111@gmail.com>
2021-06-15 23:06:15 +0200

    latest

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,20 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
 import { GetStaticProps } from 'next'
+import { usePlugin } from 'tinacms'
+import { useGithubJsonForm } from 'react-tinacms-github'
 
 export default function Home({ file }) {
-  const data = file.data
+  const formOptions = {
+    label: 'Home',
+    fields: [{
+      name: 'title',
+      component: 'text'
+    }]
+  }
+
+  const [data, form] = useGithubJsonForm(file, formOptions)
+  usePlugin(form)
 
   return (
     <div className={styles.container}>
